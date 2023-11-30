788863a352b2508d92881580732afadbc39f8548
gabarte <129518419+gabarte@users.noreply.github.com>
2023-11-30 09:33:31 -0300

    Add files via upload

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudo_membrosestaticos1
+{
+     public class ConversorDeMoeda
+    {
+        public static double Iof = 6;
+        public static double DolarParaReal(double cotacao, double quantia)
+        {
+            double total = quantia * cotacao;
+            return total + (total * Iof / 100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using estudo_membrosestaticos1;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qual a cotação do dólar? ");
+        double cotacao = double.Parse(Console.ReadLine());
+
+        Console.Write("Quantos dólares você vai comprar? ");
+        double quantia = double.Parse(Console.ReadLine());
+
+        double result = ConversorDeMoeda.DolarParaReal(cotacao, quantia);
+
+        Console.WriteLine("O valor à ser pago em real é: R$" + result.ToString("F2"));
+    }
+}
